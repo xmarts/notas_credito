0520b1061b3cd97eeffc9fe34a090ce3b57ce4b0
Pablo Osorio Gama <pablo.osorio@xmarts.com>
2019-07-02 10:38:36 -0500

    .

--- notas_credito/models/models.py
+++ notas_credito/models/models.py
@@ -7,7 +7,7 @@ from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
 class notas_credito(models.Model):
     _name = 'motivo.nota'
-    _description = "Motivos de devolución"
+    _description = "Motivos Devolución"
     name=fields.Char(string='Nombre')
 
 class AccountInvoiceRefund(models.TransientModel):
